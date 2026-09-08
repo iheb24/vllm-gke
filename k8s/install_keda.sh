60da46fc044777f38e4f76a113e40048ff86ac33
Iheb BESBES <besbesiheb@gmail.com>
2026-09-08 22:45:28 +0200

    fix: use helm upgrade --install for idempotency in install_keda.sh

--- k8s/install_keda.sh
+++ k8s/install_keda.sh
@@ -9,14 +9,14 @@ helm repo update
 
 # 2. Install KEDA
 echo "⚙️ Installing KEDA operator..."
-helm install keda kedacore/keda \
+helm upgrade --install keda kedacore/keda \
     --namespace keda \
     --create-namespace \
     --wait
 
 # 3. Install KEDA HTTP Add-on
 echo "⚙️ Installing KEDA HTTP Add-on (for 0-to-1 routing)..."
-helm install http-add-on kedacore/keda-add-ons-http \
+helm upgrade --install http-add-on kedacore/keda-add-ons-http \
     --namespace keda \
     --wait
 
